a9e54dc9cdd9f2f794b1630a0ebf4914d7f45aca
wokin <1447709546@qq.com>
2022-11-03 15:36:50 +0800

    更新文件

--- MMOItems-API/src/main/java/net/Indyuce/mmoitems/stat/Abilities.java
+++ MMOItems-API/src/main/java/net/Indyuce/mmoitems/stat/Abilities.java
@@ -119,7 +119,7 @@ public class Abilities extends ItemStat<RandomAbilityListData, AbilityListData> 
 			inv.getEditedSection().set("ability." + configKey + ".type", format);
 			inv.registerTemplateEdition();
 			inv.getPlayer().sendMessage(
-					MMOItems.plugin.getPrefix() + "成功将技能设置 " + ChatColor.GOLD + ability.getName() + ChatColor.GRAY + ".");
+					MMOItems.plugin.getPrefix() + "成功将技能设置为 " + ChatColor.GOLD + ability.getName() + ChatColor.GRAY + ".");
 			return;
 		}
 
